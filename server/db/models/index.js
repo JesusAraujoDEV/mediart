db1bf80e1c3f87e810c3d4a341e79e2ecdb83980
Jesús Alberto Araujo Arteaga <j2a0a0a5@gmail.com>
2025-06-05 20:40:21 -0400

    Estableciendo las relaciones e inicializadolas en el archivo index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,29 @@
 const { User, UserSchema } = require('./user_model');
-const { Font, FontSchema } = require('./font_model');
-const { Color, ColorSchema } = require('./color_model');
-
+const { Playlist, PlaylistSchema } = require('./playlist_model');
+const { Item, ItemSchema } = require('./items_model');
+const { UserFollow, UserFollowSchema } = require('./user_follow_model');
+const { PlaylistItem, PlaylistItemSchema } = require('./playlist_item_model');
+const { Library, LibrarySchema } = require('./library_model');
 
 function setupModels(sequelize){
   User.init(UserSchema, User.config(sequelize));
-  Font.init(FontSchema, Font.config(sequelize));
-  Color.init(ColorSchema, Color.config(sequelize));  
+  Playlist.init(PlaylistSchema, Playlist.config(sequelize));
+  Item.init(ItemSchema, Item.config(sequelize));
+  UserFollow.init(UserFollowSchema, UserFollow.config(sequelize));
+  PlaylistItem.init(PlaylistItemSchema, PlaylistItem.config(sequelize));
+  Library.init(LibrarySchema, Library.config(sequelize));
 
   User.associate(sequelize.models);
-  Font.associate(sequelize.models);
-  Color.associate(sequelize.models);
 
+  Playlist.associate(sequelize.models);
+
+  Item.associate(sequelize.models);
+
+  UserFollow.associate(sequelize.models);
+
+  PlaylistItem.associate(sequelize.models);
+
+  Library.associate(sequelize.models);
 }
 
 module.exports = { setupModels };
