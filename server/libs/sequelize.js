2a85454341760e53400e0f86cc16adba82598499
Jesús Alberto Araujo Arteaga <j2a0a0a5@gmail.com>
2025-06-17 12:33:58 -0400

    chore: Refactor logging in sequelize.js for improved clarity
    - Moved the console log for Sequelize options to after the options configuration to ensure accurate logging.
    - Maintained existing logs for environment and configuration details to aid in debugging and visibility during development.

--- server/libs/sequelize.js
+++ server/libs/sequelize.js
@@ -2,10 +2,9 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
 const { setupModels } = require('./../db/models');
 
-console.log('NODE_ENV:', process.env.NODE_ENV); // <-- Añade esto
-console.log('config.isProd:', config.isProd); // <-- Añade esto
-console.log('config.db_url:', config.db_url); // <-- Añade esto
-console.log('Sequelize options:', options); // <-- Añade esto
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('config.isProd:', config.isProd);
+console.log('config.db_url:', config.db_url);
 
 const options = {
   dialect: 'postgres',
@@ -19,6 +18,9 @@ if(config.isProd) {
     }
   }
 }
+
+// Mueve este console.log aquí, después de que 'options' esté completamente configurada
+console.log('Sequelize final options:', options);
 
 const sequelize = new Sequelize(config.db_url, options);
 
