4edd5760d77550ea4b5d8006115a6310de6f7d88
Jesús Alberto Araujo Arteaga <j2a0a0a5@gmail.com>
2025-06-17 12:31:53 -0400

    chore: Add logging for environment and configuration details in sequelize.js
    - Introduced console logs to display NODE_ENV, production status, database URL, and Sequelize options for better debugging and configuration visibility during development.

--- server/libs/sequelize.js
+++ server/libs/sequelize.js
@@ -2,6 +2,11 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
 const { setupModels } = require('./../db/models');
 
+console.log('NODE_ENV:', process.env.NODE_ENV); // <-- Añade esto
+console.log('config.isProd:', config.isProd); // <-- Añade esto
+console.log('config.db_url:', config.db_url); // <-- Añade esto
+console.log('Sequelize options:', options); // <-- Añade esto
+
 const options = {
   dialect: 'postgres',
   logging: config.isProd ? false : true,
