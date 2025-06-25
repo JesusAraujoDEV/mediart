a41fa2243a0efcf030b66db21cc4716ed1441265
Jesús Alberto Araujo Arteaga <j2a0a0a5@gmail.com>
2025-06-25 15:07:00 -0400

    feat: Add authentication schemas to Swagger documentation
    - Integrated `auth_swagger_schema` into the existing Swagger schemas, enhancing API documentation for authentication-related endpoints.
    - This addition improves the clarity and usability of the API documentation for developers working with authentication features.

--- server/swagger/components/schemas/index.js
+++ server/swagger/components/schemas/index.js
@@ -1,10 +1,12 @@
 // server/swagger/components/schemas/index.js
 
 const userSchemas = require('./user_swagger_schema');
+const authSchemas = require('./auth_swagger_schema');
 // const playlistSchemas = require('./playlist_swagger_schema'); // Cuando lo crees
 
 module.exports = {
   ...userSchemas,
+  ...authSchemas
   // ...playlistSchemas, // Descomenta cuando lo crees
   // ... (otros schemas)
 };
