4e3410dd816bfd0435153eedbe88ff482808ff42
Jesús Alberto Araujo Arteaga <j2a0a0a5@gmail.com>
2025-06-26 00:12:00 -0400

    feat: Add search schemas to Swagger documentation
    - Integrated `search_swagger_schema` into the existing Swagger schemas, enhancing API documentation for search-related endpoints.
    - This addition improves the clarity and usability of the API documentation for developers working with search features.

--- server/swagger/components/schemas/index.js
+++ server/swagger/components/schemas/index.js
@@ -2,11 +2,14 @@
 
 const userSchemas = require('./user_swagger_schema');
 const authSchemas = require('./auth_swagger_schema');
+const searchSchemas = require('./search_swagger_schema');
+
 // const playlistSchemas = require('./playlist_swagger_schema'); // Cuando lo crees
 
 module.exports = {
   ...userSchemas,
-  ...authSchemas
+  ...authSchemas,
+  ...searchSchemas
   // ...playlistSchemas, // Descomenta cuando lo crees
   // ... (otros schemas)
 };
